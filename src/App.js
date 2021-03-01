4cbfdec608a48357a7b69293b391194b849c85ac
erco <unalercann@gmail.com>
2021-03-01 13:19:07 +0300

    map implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { MapContainer, TileLayer, Circle,  } from 'react-leaflet'
+import LocationMarker from './LocationMarker'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+
+  render() {
+    const ankara = [39.891499, 32.785427]
+    const antalya = [36.391311, 32.486390]
+    const nazilli = [37.922097, 28.278020]
+    const adiyaman = [37.786415, 38.103969]
+
+    const fillBlueOptions = { color: 'blue' }
+    const redOptions = { color: 'red'}
+    return (
+      <div>
+        <MapContainer
+          center={{ lat: 39.518608, lng: 35.564017 }} 
+          zoom={7}
+          scrollWheelZoom={true}
+          style={{height:'985px'}}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Circle center={ankara} pathOptions={fillBlueOptions} radius={200} />
+          <Circle center={antalya} pathOptions={redOptions} radius={200} />
+          <Circle center={nazilli} pathOptions={redOptions} radius={200} />
+          <Circle center={adiyaman} pathOptions={fillBlueOptions} radius={200} />
+        </MapContainer>
+      </div>
+    )
+  }
 }
-
-export default App;
